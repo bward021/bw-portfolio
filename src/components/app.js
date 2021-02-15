4bc7608099e5aa8ebbb6c166456df688e9af1d25
Brandon Ward <brandonward.gk@gmail.com>
2021-02-15 16:26:15 -0700

    Introduction to React Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import moment from 'moment';
 
+import PortfolioContainer from "./portfolio-container"
+
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Brandon Ward Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
