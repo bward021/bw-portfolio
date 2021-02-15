bf900a20453b175eaa719512e1f88cc6e4e2194c
Brandon Ward <brandonward.gk@gmail.com>
2021-02-15 12:02:41 -0700

    initial commit for Portfolio

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Brandon Ward Portfolio</h1>
+      </div>
+    );
+  }
+}
