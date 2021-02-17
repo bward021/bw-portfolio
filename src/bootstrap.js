9a23cd8aff9e3d16108ddc60a11e65cd79b5d86b
Brandon Ward <brandonward.gk@gmail.com>
2021-02-17 14:24:40 -0700

    update to Browser Router

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
 import App from "./components/app";
 import reducers from "./reducers";
 
@@ -13,9 +12,7 @@ import "./style/main.scss";
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
   );
