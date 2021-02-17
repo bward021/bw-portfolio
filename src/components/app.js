3386628dd1fa2a45b649c1cbddcc771ffb83e667
Brandon Ward <brandonward.gk@gmail.com>
2021-02-17 12:19:32 -0700

    Basic Route Setup in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
-import moment from 'moment';
+import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container"
-import NavigationContainer from "./navigation/navigation-container"
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavigationContainer />
+
+        <Router>
+          <div>
+          <NavigationContainer />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about-me" component={About}/>
+          </Switch>
+          </div>
+        </Router>
+
         <h1>Brandon Ward Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
       </div>
     );
