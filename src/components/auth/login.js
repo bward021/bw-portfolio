82ce0eb012889675ce900d2e2a89a52f1cfa0005
Brandon Ward <brandonward.gk@gmail.com>
2021-02-19 10:17:01 -0700

    handling of form input add

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+  handleSubmit(event) {
+    console.log("handle submit:", event)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
-       <div>
-         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-         <form action="">
-           <input type="text"/>
-           <input type="password"/>
-         </form>
-       </div>
-    )
+      <div>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Your Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    );
   }
-};
+}
