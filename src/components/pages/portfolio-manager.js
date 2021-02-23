d4f67040594be80e772479f43024a43927bc1996
Brandon Ward <brandonward.gk@gmail.com>
2021-02-23 14:19:54 -0700

    populating the portfolio manager with api data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,12 +1,44 @@
-import React, { Component } from 'react'
+import axios from "axios";
+import React, { Component } from "react";
+import axior from "axios";
 
 export default class PortfolioManager extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      isLoading: false,
+      portfolioItems: []
+    }
+  }
+
+  getPortfolioItems() {
+    axios.get('https://bward.devcamp.space/portfolio/portfolio_items', {withCredentials: true})
+      .then(res => {
+        this.setState({
+          isLoading: false,
+          portfolioItems: [...res.data.portfolio_items]
+        })
+      })
+      .catch(err => {
+        console.log("Error: ", err)
+      })
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
-       <div>
-         <h1>PortfolioManager</h1>
-         <h1>PortfolioManager</h1>
-       </div>
-    )
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>PortfolioForm...</h1>
+        </div>
+        <div className="right-column">
+          <h1>Portfolio SideBar...</h1>
+        </div>
+      </div>
+    );
   }
-};
+}
