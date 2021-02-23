80f885f125c75999287fc28870761fad7f3c26e2
Brandon Ward <brandonward.gk@gmail.com>
2021-02-23 12:24:33 -0700

    finished sign out component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,40 +1,67 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { withRouter } from "react-router"
 
 const NavigationComponent = (props) => {
   const dynamicLink = (route, linkText) => {
-    return(
-      <div className="nav-link-wrapper">
-      <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-    </div>
-    )
-  }
-
-
     return (
-      <div className="nav-wrapper">
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-          </div>
-          <div className="nav-link-wrapper">  
-            <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-          </div>
-
-          {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
-
-          {/* <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-          </div> */}
-        </div>
-        <div className="right-side">
-          Brandon Ward
-        </div>
+      <div className="nav-link-wrapper">
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
       </div>
     );
-}
+  };
 
-export default NavigationComponent;
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then((response) => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("Error signing out", error);
+      });
+  };
+
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
+
+        {props.loggedInStatus === "LOGGED_IN"
+          ? dynamicLink("/blog", "Blog")
+          : null}
+
+        {/* <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+          </div> */}
+      </div>
+      <div className="right-side">
+        <p>Brandon Ward</p>
+        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null } 
+        </div>
+    </div>
+  );
+};
+
+export default withRouter(NavigationComponent);
