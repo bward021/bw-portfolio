fcb1946042c008e8644451d5d3e96cb1aa14901a
Brandon Ward <brandonward.gk@gmail.com>
2021-02-24 13:26:01 -0700

    creating a portfolio Items from react Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    
+
     this.state = {
       name: "",
       description: "",
@@ -12,8 +13,8 @@ export default class PortfolioForm extends Component {
       url: "",
       thumb_img: "",
       banner_image: "",
-      logo: ""      
-    }
+      logo: "",
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,17 +23,27 @@ export default class PortfolioForm extends Component {
   buildForm() {
     let formData = new FormData();
 
-    formData.append("portfolio_item[name]", this.state.name)
-    formData.append("portfolio_item[description]", this.state.description)
-    formData.append("portfolio_item[url]", this.state.url)
-    formData.append("portfolio_item[category]", this.state.category)
-    formData.append("portfolio_item[position]", this.state.position)
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
 
     return formData;
   }
 
   handleSubmit(event) {
-    this.buildForm();
+    // https://bward.devcamp.space/portfolio/portfolio_items
+    axios.post(
+      "https://bward.devcamp.space/portfolio/portfolio_items",
+      this.buildForm(),
+      { withCredentials: true }
+    ) .then(response => {
+      console.log("response: ", response);
+    }) .catch(error => {
+      console.log("portfolio form submit error: ", error);
+    })
+    
     event.preventDefault();
     console.log("event: ", event);
   }
@@ -40,62 +51,61 @@ export default class PortfolioForm extends Component {
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
-      errorText: ""
-    })
+      errorText: "",
+    });
   }
-
 
   render() {
     return (
-       <div>
-         <h1>PortfolioForm</h1>
-         <form onSubmit={this.handleSubmit}>
-           <div>
-            <input 
+      <div>
+        <h1>PortfolioForm</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
               type="text"
               name="name"
               placeholder="Portfolio Item Name"
               value={this.state.name}
               onChange={this.handleChange}
             />
-            <input 
-                type="text"
-                name="url"
-                placeholder="URL"
-                value={this.state.url}
-                onChange={this.handleChange}
+            <input
+              type="text"
+              name="url"
+              placeholder="URL"
+              value={this.state.url}
+              onChange={this.handleChange}
             />
-            </div>
-           <div>
-           <input 
+          </div>
+          <div>
+            <input
               type="text"
               name="position"
               placeholder="Position"
               value={this.state.position}
               onChange={this.handleChange}
             />
-            <input 
-                type="text"
-                name="category"
-                placeholder="Category"
-                value={this.state.category}
-                onChange={this.handleChange}
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={this.state.category}
+              onChange={this.handleChange}
             />
-           </div>
-           <div>
-             <input
-               type="text"
-                name="description"
-                placeholder="Description"
-                value={this.state.description}
-                onChange={this.handleChange}
-              />
-           </div>
-           <div>
-             <button type="submit" >Save</button>
-           </div>
-         </form>
-       </div>
-    )
+          </div>
+          <div>
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Save</button>
+          </div>
+        </form>
+      </div>
+    );
   }
-};
+}
--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -4,7 +4,7 @@ const portfolioSideBarList = (props) => {
 
   const portfolioList = props.data.map(portfolioItem => {
     return (
-      <div className="portfolio-item-thumb">
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
         <div className="portfolio-thumb-img">
           <img src={portfolioItem.thumb_image_url} alt=""/>
         </div>
