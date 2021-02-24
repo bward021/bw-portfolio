82223c414fcf2668ec1b6e1f76d05c8ff9a876d0
Brandon Ward <brandonward.gk@gmail.com>
2021-02-24 11:59:59 -0700

    introduction to building javascript formData

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,7 +19,20 @@ export default class PortfolioForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name)
+    formData.append("portfolio_item[description]", this.state.description)
+    formData.append("portfolio_item[url]", this.state.url)
+    formData.append("portfolio_item[category]", this.state.category)
+    formData.append("portfolio_item[position]", this.state.position)
+
+    return formData;
+  }
+
   handleSubmit(event) {
+    this.buildForm();
     event.preventDefault();
     console.log("event: ", event);
   }
