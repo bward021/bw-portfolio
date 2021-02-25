12cb1dfd1b84b18dfe13f2a8fc7e52aa659d84e1
Brandon Ward <brandonward.gk@gmail.com>
2021-02-25 14:26:18 -0700

    How to Style Font Awesome Icons in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 const portfolioSideBarList = (props) => {
-
-  const portfolioList = props.data.map(portfolioItem => {
+  const portfolioList = props.data.map((portfolioItem) => {
     return (
       <div key={portfolioItem.id} className="portfolio-item-thumb">
         <div className="portfolio-thumb-img">
-          <img src={portfolioItem.thumb_image_url} alt=""/>
+          <img src={portfolioItem.thumb_image_url} alt="" />
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => {props.handleDeleteClick(portfolioItem)}}><FontAwesomeIcon icon="trash" /> </a>
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          <a
+            className="delete-icon"
+            onClick={() => {
+              props.handleDeleteClick(portfolioItem);
+            }}
+          >
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
       </div>
-    )
+    );
   });
 
-  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>
-}
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+};
 
 export default portfolioSideBarList;
