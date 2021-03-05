e39cb25d86f26518d19a2dc11f5ecc48fbde3658
Brandon Ward <brandonward.gk@gmail.com>
2021-03-05 13:40:55 -0700

    updated bio in about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,41 +1,34 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/about/profilepicture.jpg"
+import profilePicture from "../../../static/assets/images/about/profilepicture.jpg";
 
 export default function () {
   return (
     <div className="content-page-wrapper">
-      <div className="left-column" 
+      <div
+        className="left-column"
         style={{
-          background:"url(" + profilePicture + ") no-repeat",
+          background: "url(" + profilePicture + ") no-repeat",
           backgroundSize: "cover",
-          backgroundPosition: "Center"
-        }}>
-      </div>
+          backgroundPosition: "Center",
+        }}
+      ></div>
       <div className="right-column">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        consectetur sed delectus voluptates, eum totam eaque quas commodi minima
-        ipsam fugit aut obcaecati quod nisi omnis harum illum laboriosam
-        temporibus. Iusto harum mollitia rerum voluptatum doloribus voluptate
-        placeat dicta aspernatur est voluptatibus cum, cupiditate et? Ipsum
-        magnam animi recusandae fugit reprehenderit provident, iusto nobis
-        eligendi, excepturi ut explicabo! Harum, consequuntur! Rerum dolorum
-        consequatur blanditiis assumenda dicta, aperiam, aliquid ab facilis
-        quisquam excepturi numquam at dolore iusto nesciunt quo sed provident ea
-        officiis fugiat mollitia aut ducimus repudiandae! Tempore, omnis
-        eveniet. Consectetur rem sapiente quia? Voluptate inventore voluptatibus
-        ut aut veniam magnam pariatur alias reiciendis, eius quisquam repellat
-        quibusdam nihil et beatae dignissimos autem? Voluptate impedit ipsum,
-        nemo eligendi esse dolore. Accusamus, perferendis mollitia facilis illo
-        sunt ad tempora placeat iste consectetur officiis quae in. Accusamus
-        veritatis facilis magnam, earum consequatur vitae necessitatibus error
-        voluptatem exercitationem nam eveniet, reiciendis ea deserunt?
-        Similique, explicabo! Provident quo nihil minus eligendi esse pariatur
-        odit laborum aliquid quos. In quod, debitis mollitia voluptatem
-        reprehenderit molestiae libero voluptate necessitatibus dicta id
-        adipisci harum nam eveniet eum. Facilis vitae natus enim illo cumque
-        suscipit eveniet obcaecati laborum tenetur, nostrum totam quas iusto,
-        sed veniam provident! Rem placeat culpa excepturi natus! Maxime, facilis
-        eos!
+        <p>
+          Hey there, my name is Brandon! I have come to learn that we live in a
+          day and age where things are changing. Constantly! Now, some people
+          would take a look at that statement and have an instant flood of
+          negative emotion. <b>NOT ME!</b>
+        </p>
+        I revel in the thought of change, because change means growth. If we are
+        stuck in the everyday rhythm of the "Box" that is called complacency, we
+        can never be more than who we are. I am constantly looking to be more!
+        To learn more, to grow more and to better myself every single day.
+        <p>
+          I am currently looking to work for a company that will help me be a
+          better version of me. Who in return will recieve a very motivated and
+          passionate employee. I work until the work is done and know that I can
+          be a benefit to you and your company!
+        </p>
       </div>
     </div>
   );
